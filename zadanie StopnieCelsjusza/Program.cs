77dc31f95696dd2d6a839a0ed8e98f0db30c57f2
pawel <pawel@KULAWEPC>
2023-05-03 18:29:46 +0100

    Dodaj pliki projektów.

---
+++ zadanie StopnieCelsjusza/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace zadanie_StopnieCelsjusza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperatura pierwsza= new Temperatura();
+            pierwsza.stopnieCelsjusza= 0;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", pierwsza.stopnieKelwina , pierwsza.stopnieCelsjusza, pierwsza.StopnieFahrenheita);
+            Temperatura druga = new Temperatura();
+            druga.stopnieCelsjusza= 100;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", druga.stopnieKelwina, druga.stopnieCelsjusza, druga.StopnieFahrenheita);
+            Temperatura trzecia = new Temperatura();
+            trzecia.stopnieCelsjusza= 36.6;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", trzecia.stopnieKelwina, trzecia.stopnieCelsjusza, trzecia.StopnieFahrenheita);
+            Temperatura czwarta = new Temperatura();
+            czwarta.stopnieKelwina2 = 0;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", czwarta.stopnieKelwina2, czwarta.stopnieCelsjusza2, czwarta.StopnieFahrenheita);
+            Temperatura piąta = new Temperatura();
+            piąta.StopnieFahrenheita2 = 0;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", piąta.stopnieKelwina, piąta.stopnieCelsjusza3, piąta.StopnieFahrenheita2);
+            Temperatura szósta = new Temperatura();
+            szósta.StopnieFahrenheita2 = 100;
+            Console.WriteLine("Temperatura w Kelwinach wynosi:{0}, w stopniach Celsjusza: {1}, a w stopniach Fahrenheita {2}", szósta.stopnieKelwina, szósta.stopnieCelsjusza3, szósta.StopnieFahrenheita2);
+        }
+
+    }
+}
---
+++ zadanie StopnieCelsjusza/Temperatura.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_StopnieCelsjusza
+{
+    public class Temperatura
+    {
+        
+        private double stopniekelwina;
+        private double stopniefahrenheita;
+        private double stopniecelsjusza;
+        public double stopnieCelsjusza; //jednostka bazowa Celsjusz
+        public double StopnieFahrenheita2;//jednostka bazowa Fahrenheita
+        public double stopnieKelwina2;//jednostka bazowa Kelwin
+        public double stopnieCelsjusza2//konwersja celsjusza na kelwina
+        {
+            get
+            {
+                stopniecelsjusza = stopnieKelwina2 - 273.15;
+                return stopniecelsjusza;
+            }
+        }
+        public double stopnieKelwina //konwersja Fahrenheit na Kelwina
+        {
+            
+            get 
+            {
+                stopniekelwina = ((StopnieFahrenheita2-32)/1.8) + 273.15;
+                return stopniekelwina;
+            }
+            set
+            {
+                if (stopniekelwina<0)
+                {
+                    throw new ArgumentException("Temperatura musi być większa od zera.");
+                }
+            }
+        }
+        public double StopnieFahrenheita //Celsjusz na Fahrenheit
+        {
+            get
+            {
+                stopniefahrenheita = (2*stopnieCelsjusza)+32;
+                return stopniefahrenheita;
+             }
+            set
+            {
+                if (StopnieFahrenheita<0)
+                {
+                    throw new ArgumentException("Temperatura musi być większa od zera.");
+                }
+            }
+        }
+        public double stopnieCelsjusza3 //Fahrenheit na Celsjusza
+        {
+            get
+            {
+                stopniecelsjusza = (StopnieFahrenheita2-32)/1.8;
+                return stopniecelsjusza;
+            }
+        }
+    }
+}
+
